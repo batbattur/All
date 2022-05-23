803d6d8e09aff01f72aae44a578f21584a61dbc5
batbattur <58952979+batbattur@users.noreply.github.com>
2022-05-22 19:56:47 -0700

    Update testing.sh

--- testing.sh
+++ testing.sh
@@ -1,3 +1,22 @@
 #!/bin/bash -ex
-echo "TESTING BASH"
-exit 1
+inside() {
+   echo "inside"
+   exit 1
+}
+
+outside() {
+   inside
+   echo "outside"
+}
+
+
+# Check if the function exists (bash specific)
+if declare -f "$1" > /dev/null
+then
+  # call arguments verbatim
+  "$@"
+else
+  # Show a helpful error
+  echo "'$1' is not a known function name" >&2
+  exit 1
+fi
