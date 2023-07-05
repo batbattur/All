0f6ad41dba4b5e08088434a59e16cbb1a6cb0096
batbattur <bat@ifixit.com>
2023-07-05 13:48:15 -0700

    use past cron date

--- .github/renovateConfig.js
+++ .github/renovateConfig.js
@@ -26,7 +26,7 @@ module.exports = {
          "description": "Disabled Paths",
          "matchPaths": ["Testing/**"],
          // "dependencyDashboardApproval": true
-         "schedule": ["never"]
+         "schedule": ["0 0 0 1 1 ? 1970"]
       }
    ],
    "rebaseWhen": "conflicted",
